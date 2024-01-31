a4a7bbb778eb88f8630325dec5092e81845b2cd2
Juriy_Strogg <Strog1998@gmail.com>
2024-01-31 22:20:17 +0500

    functions for generating photos and users

--- js/main.js
+++ js/main.js
@@ -19,3 +19,60 @@ function getRandomNumber(min, max) {
 function validateLength(string, maxLength) {
   return string.length <= maxLength;
 }
+
+//Комментарии и имена
+
+let allNames = ["Антон", "Виктор", "Дмитрий", "Евгений", "Женя", "Коля"];
+let allComments = [
+  "Всё отлично!",
+  "В целом всё неплохо. Но не всё.",
+  "Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.",
+  "Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.",
+  "Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.",
+  "Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!",
+];
+
+//Генератор комментариев и имен
+
+function createComments() {
+  const comments = [];
+  for (let i = 0; i < 10; i++) {
+    comments.push({
+      id: getRandomNumber(1, 100000),
+      avatar: `img/avatar-${getRandomNumber(1, 6)}.svg`,
+      message: allComments[getRandomNumber(0, allComments.length - 1)],
+      name: allNames[getRandomNumber(0, allNames.length - 1)],
+    });
+  }
+  return comments;
+}
+
+//Генератор описания фотографий
+let photoDescriptions = [
+  "Тестим новую камеру!",
+  "Замечательные фотки!",
+  "Очень красивые фотки!",
+  "Супер красивые фотки!",
+  "Красивые фотки!",
+];
+function createPhotoDescription() {
+  return photoDescriptions[getRandomNumber(0, photoDescriptions.length - 1)];
+}
+
+//Генератор фотографий
+
+function createPhotos() {
+  const photos = [];
+  for (let i = 0; i < 25; i++) {
+    photos.push({
+      id: getRandomNumber(1, 25),
+      url: `photos/${getRandomNumber(1, 25)}.jpg`,
+      description: createPhotoDescription(),
+      likes: getRandomNumber(15, 200),
+      comments: createComments(),
+    });
+  }
+  return photos;
+}
+
+console.log(createPhotos());
